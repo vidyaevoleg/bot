8ad13429727f794de4568f649aed6bb886a65356
vidayevoleg <olezhka-vidyayev@mail.ru>
2017-10-22 04:13:26 +0300

    sorting sessions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ClientController
 
   def show
     account = Account.eager_load(:templates).find(params[:id])
-    sessions = Account::Session.includes(:template).where(account_template_id: account.templates.pluck(:id)).reverse
+    sessions = Account::Session.includes(:template).where(account_template_id: account.templates.pluck(:id)).order(id: :desc).limit(100)
     gon.account = AccountSerializer.new(account).as_json
     gon.sessions = sessions.map {|s| SessionSerializer.new(s).as_json}
   end

f5858ff8bfb32fd3db67b4e8992ffd7884d33c05
vidayevoleg <olezhka-vidyayev@mail.ru>
2017-10-30 18:44:08 +0300

    merge hotfix

--- app/services/strategy/rank_volume.rb
+++ app/services/strategy/rank_volume.rb
@@ -2,7 +2,7 @@ class Strategy::RankVolume < Strategy
 
   def get_summaries
     sums = client.summaries.all.find_all {|s| s.market.include?("#{currency}")}
-    white_listed = sums.select {|s| settings.white_list.include?(s.market)}
+    white_listed = sums.select {|s| template.white_list.include?(s.market)}
     by_rank = sums.sort {|s| (s.spread * s.volume ) }
     (white_listed + by_rank).uniq
   end

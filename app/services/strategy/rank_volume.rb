60978a6e02fcb982aceae6c2ebabdef8bc4a095e
vidayevoleg <olezhka-vidyayev@mail.ru>
2017-10-31 14:49:13 +0300

    fix rank volume strategy

--- app/services/strategy/rank_volume.rb
+++ app/services/strategy/rank_volume.rb
@@ -1,4 +1,4 @@
-class Strategy::RankVolume < Strategy
+class Strategy::RankVolume < Strategy::QuickSell
   def get_summaries
     sums = client.summaries.all.find_all {|s| s.market.include?("#{currency}")}
     white_listed = sums.select {|s| template.white_list.include?(s.market)}
